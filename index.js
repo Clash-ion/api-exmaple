d882add16549891cdff0c6c608e7b137e7d8e16b
Mukul Kumar <mukul0000kumar@gmail.com>
2022-04-01 23:22:38 +0530

    add files

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const bodyParser = require("body-parser");
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.get("/", (req,res) =>{
+    res.json({"hello" : "hi"})
+})
+
+app.post("/api/v1/takeEmail/", (req,res) => {
+    
+    var {email} = req.body
+    console.log(email)
+    res.status(200).json({
+        "success" : true,
+        body : {
+            "emailRecieved" : email       
+        }
+    })
+})
+
+app.listen(PORT, () => {
+    console.log('server started at ${PORT}!')
+})
+// import express from "express"
